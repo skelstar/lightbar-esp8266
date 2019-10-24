8d11896ff6f37cfeb79b47f32ae0a98452d5a1b4
Sean <skelstar@gmail.com>
2019-10-24 17:27:46 +1300

    Leds working now with ESP32.

---
+++ include/leds.h
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+
+#define NUM_PIXELS  21
+#define PIXEL_PIN   4
+#define BRIGHT_MAX  10
+
+CRGB strip[NUM_PIXELS];
+
+uint32_t COLOUR_OFF = CRGB::Black;
+uint32_t COLOUR_WHITE = CRGB::White;
+uint32_t COLOUR_RED = CRGB::Red;
+
+
+void allLedsOn(uint32_t colour) {
+  for (int i=0; i<NUM_PIXELS; i++) {
+    strip[i] = colour;
+  }
+  FastLED.show();
+}
+
+void fadeall()
+{
+  for (int i = 0; i < NUM_PIXELS; i++)
+  {
+    strip[i].nscale8(250);
+  }
+}
+
+void initialiseLeds() {
+  FastLED.addLeds<WS2812B, PIXEL_PIN, GRB>(strip, NUM_PIXELS);
+  FastLED.setBrightness(50);
+  allLedsOn(COLOUR_WHITE);
+  FastLED.show();
+}
