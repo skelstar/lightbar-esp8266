a13c5b1cc859b24138c5445459aad5f16915d5d6
Sean <skelstar@gmail.com>
2019-10-25 09:10:46 +1300

    moving, stopped states working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 #include <TaskScheduler.h>
+#include <Fsm.h>
 
 #include "leds.h"
 #include "vesc.h"
 
+
+enum EventsEnum
+{
+  POWERED_UP,
+  MOVING,
+  STOPPED_MOVING
+} event;
+
+//-------------------------------
+State state_poweredup(
+  [] { Serial.printf("state_poweredup \n"); },
+  [] { 
+    drawBatteryPercent(batteryVoltage);
+  }, 
+  NULL
+);
+
+State state_moving(
+  [] { 
+    Serial.printf("state_moving \n");
+    allLedsOn(COLOUR_WHITE);
+  }, 
+  NULL, 
+  NULL
+);
+
+State state_stopped(
+  [] { Serial.printf("state_stopped \n"); },
+  [] { 
+    drawBatteryPercent(batteryVoltage);
+  }, 
+  NULL
+);
+
+Fsm fsm(&state_poweredup);
+
+void addFsmTransitions() {
+
+  uint8_t event = POWERED_UP;
+  // fsm.add_transition(&state_battery_voltage_screen, &state_server_disconnected, event, NULL);
+
+  event = MOVING;
+  fsm.add_transition(&state_poweredup, &state_moving, event, NULL);
+  fsm.add_transition(&state_stopped, &state_moving, event, NULL);
+  
+
+  event = STOPPED_MOVING;
+  // when state connected is entered it will transition to new state after 3 seconds
+  fsm.add_timed_transition(&state_moving, &state_stopped, 3000, NULL);
+}
 
 Scheduler runner;
 
@@ -24,7 +75,14 @@ Task t_GetVescValues(
       {
         Serial.printf("Batt: %.1f\n", batteryVoltage);
         int8_t pc = getBatteryPercentage(batteryVoltage);
-        drawBatteryPercent(pc);
+        if (moving) {
+          fsm.trigger(MOVING);
+        }
+        else {
+          fsm.trigger(STOPPED_MOVING);
+        }
+        // drawBatteryPercent(pc);
+        fsm.run_machine();
       }
       // fsm.run_machine();
     });
@@ -33,6 +91,9 @@ Task t_GetVescValues(
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n\nready!\n");
+
+  addFsmTransitions();
+  fsm.run_machine();
   
   vesc.init(VESC_UART_BAUDRATE);
 
@@ -46,6 +107,8 @@ void setup() {
 void loop() {
   
   runner.execute();
+
+  // fsm.run_machine();
 
   delay(10);
 }
